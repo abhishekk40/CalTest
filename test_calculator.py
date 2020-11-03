b99f06c6578fdc4200395656bf06afd3fc80105c
abhishekk40 <66176753+abhishekk40@users.noreply.github.com>
2020-11-03 17:57:09 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -11,11 +11,13 @@ class TestCalculator(unittest.TestCase):
     def test_addition(self):
         result = calculator.add(2, 6)
         self.assertEqual(result, 8)
+        print("Unit test of addition completed")
         
 
     def test_subtraction(self):
         result = calculator.subtract(4,1)
         self.assertEqual(result, 3)
+        print("Unit test of substraction completed")
         
         
 if __name__ == '__main__':
